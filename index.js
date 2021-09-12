5700db9d41b12d4035736a525fe99e79dcfdf6cb
github username <github email address>
2021-09-12 14:18:44 -0500

    commit tree efe447eea010179b3bcb87305933b5b8cab13a37

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+ const multiply = num1 * num2
+const random = Math.floor(Math.random()*1000)+1
+let num3 = 16; let num4= 6;
+const mod = num3%num4
+const max = Math.max(1,2,3,6,9,20)
